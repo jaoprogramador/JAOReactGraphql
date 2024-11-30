70bf583fac8cce03c1ee0933b06f17ab923f8b2f
jao <jaoprogramador@gmail.com>
2024-11-30 18:55:49 +0100

    Error en favoritos

--- index.mjs
+++ index.mjs
@@ -3,7 +3,8 @@ const JWT_SECRET = 'SECRET_KEY';
 //const { GraphQLError } = require('graphql');
 import { GraphQLError } from 'graphql';
 import { createServer } from 'http';
-const { startStandaloneServer } from '@apollo/server/standalone';
+import { startStandaloneServer } from '@apollo/server/standalone';
+
 import { ApolloServer } from '@apollo/server';
 import { expressMiddleware } from '@apollo/server/express4';
 import { makeExecutableSchema } from '@graphql-tools/schema';
