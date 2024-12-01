cfabad815931ad9ab4163dd0a69043ae8194bfeb
jao <jaoprogramador@gmail.com>
2024-12-01 16:14:35 +0100

    Error: GraphQL operations must contain6

--- index.mjs
+++ index.mjs
@@ -8,7 +8,7 @@ import { expressMiddleware } from '@apollo/server/express4';
 import { makeExecutableSchema } from '@graphql-tools/schema';
 import WebSocket from 'ws';  // Importación por defecto de 'ws'
 const { WebSocketServer } = WebSocket;
-import { useServer } from 'graphql-ws';
+import { useServer } from 'graphql-ws/lib/use/ws';
 
 
 import express from 'express';
